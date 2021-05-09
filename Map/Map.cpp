9eb3a6d2bd31733b22d77ed4bb2ab55efb1fb280
Nataliya <nmotorygina@yandex.ru>
2021-05-10 01:14:57 +0300

    Версия 2

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -15,51 +15,50 @@ int main()
 	int grade, pos;
 	vector <int> grades;
 	StudentsAll studentsAll;
-	
+
 	ifstream file;
 	file.open("Test.txt");
+
 	if (file.is_open()) {
 		while (!file.eof()) {
-			grades.clear();
 			getline(file, line, ';');
 
+			while (line.find("\n") != string::npos)
+			{
+				pos = line.find("\n");
+				line.erase(pos, 1);
+			}
+
 			if (!line.empty()) {
-
-			pos = line.find("-");
-			name = line.substr(0, pos);
-			line = line.substr(pos + 1);
-
-			while (line.find(",") != string::npos)
-			 {
-				pos = line.find(",");
-				temp = line.substr(0, pos);
-				grade = stoi(temp);
+				pos = line.find("-");
+				name = line.substr(0, pos);
 				line = line.substr(pos + 1);
+
+				while (line.find(",") != string::npos)
+				{
+					pos = line.find(",");
+					temp = line.substr(0, pos);
+					grade = stoi(temp);
+					line = line.substr(pos + 1);
+					grades.push_back(grade);
+				}
+
+				grade = stoi(line);
 				grades.push_back(grade);
 			}
-				
-			grade = stoi(line);
-			grades.push_back(grade);
 
-			}
-
-		auto element = Student(name, grades);
+			auto element = Student(name, grades);
 			studentsAll.insert(element);
-
+			grades.clear();
 		}
 	}
 	else cout << "Error!";
-	
-	for (auto item : studentsAll) {	
-		cout << item.first;
 
+	for (auto item : studentsAll) {
+		cout << item.first;
 		for (auto grade : item.second) {
-			cout << grade << ", ";
+			cout << " - " << grade;
 		}
 		cout << endl;
-		
-		
 	}
-
-
 }
